b39378a2273769f499f17d59590a7219676810db
iamsurapat12 <iamsurapat12@outlook.co.th>
2020-09-11 23:56:55 +0700

    Add project files.

---
+++ fifthweek3/Source.cpp
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+void multipleTable(int k)
+{
+	int a[25][25], i, j, x,m;
+	for (i = 1; i <= k; i++)
+	{
+		for (j = 1; j <= k; j++)
+		{
+			x = i - 1;
+			a[x][j - 1] = i * j;
+
+		}
+	}
+	int n = 1;
+	printf("*    |");
+	for (n = 1; n <= k; n++)
+	{
+		printf("\t%d", n);
+	}
+	printf("\n");
+	printf("-------------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
+	for (i = 0; i < k; i++)
+	{
+		printf("%d    |\t", i + 1);
+		for (j = 0; j < k; j++)
+		{
+			printf("%d\t", a[i][j]);
+
+		}
+		printf("\n");
+
+	}
+
+}
+int main()
+{
+	int m;
+	printf("Enter the number (no more than 20) : ");
+	scanf_s("%d", &m);
+	multipleTable(m);
+	return 0;
+}
